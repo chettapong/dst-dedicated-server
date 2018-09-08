41ffe4736e7b3724b0aa0bf0a96a927bc9e0a628
Caio Mathielo <mathielo@gmail.com>
2018-09-08 12:39:05 +0200

    Rename Cluster folder

--- start-container-server.sh
+++ start-container-server.sh
@@ -5,19 +5,19 @@
 /home/dst/steamcmd.sh +@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 +login anonymous +force_install_dir /home/dst/server_dst +app_update 343050 validate +quit
 
 # Copy dedicated_server_mods_setup.lua
-ds_mods_setup="$HOME/.klei/DoNotStarveTogether/DSTWhalesCluster/mods/dedicated_server_mods_setup.lua"
+ds_mods_setup="$HOME/.klei/DoNotStarveTogether/Cluster/mods/dedicated_server_mods_setup.lua"
 if [ -f "$ds_mods_setup" ]
 then
   cp $ds_mods_setup "$HOME/server_dst/mods/"
 fi
 
 # Copy modoverrides.lua
-modoverrides="$HOME/.klei/DoNotStarveTogether/DSTWhalesCluster/mods/modoverrides.lua"
+modoverrides="$HOME/.klei/DoNotStarveTogether/Cluster/mods/modoverrides.lua"
 if [ -f "$modoverrides" ]
 then
-  cp $modoverrides "$HOME/.klei/DoNotStarveTogether/DSTWhalesCluster/Master/"
-  cp $modoverrides "$HOME/.klei/DoNotStarveTogether/DSTWhalesCluster/Caves/"
+  cp $modoverrides "$HOME/.klei/DoNotStarveTogether/Cluster/Master/"
+  cp $modoverrides "$HOME/.klei/DoNotStarveTogether/Cluster/Caves/"
 fi
 
 cd $HOME/server_dst/bin
-./dontstarve_dedicated_server_nullrenderer -cluster DSTWhalesCluster -shard "$SHARD_NAME"
+./dontstarve_dedicated_server_nullrenderer -cluster Cluster -shard "$SHARD_NAME"
